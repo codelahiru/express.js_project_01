30f716a05375d9ebb430cfe20fda9a086aecd95b
codelahiru <gayan.hatharasinghe@gmail.com>
2023-07-17 21:34:39 +0530

    071723-21.34

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('node:path');
 
 const app = express();
 
 // create a route
 app.get('/', (req, res) => {           // 'req' is request and 'res' is response
-    res.send('<h1>Hello World!!!</h1>');
+    //res.send('<h1>Hello World!!!</h1>');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
 const PORT = process.env.PORT || 5000;
